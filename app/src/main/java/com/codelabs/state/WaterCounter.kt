b8cdf33e3445a67528e7e97682b9625f42e88d8d
DanishZDN <muhammad.danish@student.umn.ac.id>
2025-02-11 00:29:48 +0700

    step 5 ???????????????????? EROR ?

--- app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -26,14 +26,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.MutableState
 
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text("You've had $count glasses of milk🐀.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        val count: MutableState<Int> = mutableStateOf(0)
+
+        Text("You've had ${count.value} glasses.")
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
